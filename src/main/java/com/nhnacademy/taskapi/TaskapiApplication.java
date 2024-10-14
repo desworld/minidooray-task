07991ee3e2dcca53e09de4af7c13cb4a653c0696
kimminjun <jj36282013@gmail.com>
2024-10-14 22:36:46 +0900

    init

--- src/main/java/com/nhnacademy/taskapi/TaskapiApplication.java
+++ src/main/java/com/nhnacademy/taskapi/TaskapiApplication.java
@@ -2,8 +2,10 @@ package com.nhnacademy.taskapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TaskapiApplication {
 
     public static void main(String[] args) {
